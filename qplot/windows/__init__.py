df80ddef4b6eb33259d266ab27544514c42dae6a
Wordsbo <b.wordsworth03@gmail.com>
2025-07-08 16:07:11 +0100

    Early Static Functional
    Read from static database and output graphs.

---
+++ qplot/windows/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  6 16:31:54 2025
+
+@author: Benjamin Wordsworth
+"""
+
+from .plot1d import plot1d
+from .plot2d import plot2d
+
+__all__ = [
+    "plot1d",
+    "plot2d",
+    ]
---
+++ qplot/windows/plot1d.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  6 16:35:38 2025
+
+@author: Benjamin Wordsworth
+"""
+from PyQt5 import QtWidgets as qtw
+
+import qcodes
+
+import numpy as np
+
+from qplot.tools import unpack_param
+from .setup import plotWidget
+
+class plot1d(plotWidget):
+    def __init__(self, 
+                 dataset : qcodes.dataset.data_set.DataSet, 
+                 param : qcodes.dataset.ParamSpec
+                 ):
+        super().__init__(dataset, param, str(dataset.run_id))
+        
+        print("Working")
+        
+        
+        
+        indepParam = unpack_param(dataset, param.depends_on)
+        print()
+        
+        
+        if isinstance(self.df.index.names, type(None)) or len(self.df.index.names) == 1:
+            indepData = self.df.index.to_numpy(float)
+        else: 
+            name_ind = self.df.index.names.index(indepParam.name)
+            unpacked_index = np.array(
+                [*self.df.index], #unpack tupple to produce nd array
+                dtype=float
+                )
+            indepData = unpacked_index[:,name_ind]
+            
+        
+        
+        plot = self.widget.addPlot(x=indepData, y=self.depvarData)
+        plot.setLabel(axis="bottom", text=f"{indepParam.label} ({indepParam.unit})")
+        plot.setLabel(axis="left", text=f"{param.label} (param.unit)")
+        
+        print("Graph produced \n")
+        
+        
+        
+        
