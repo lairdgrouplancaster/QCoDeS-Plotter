de2ba59913eb48c18dc2b2c9992c53ea00f80732
Wordsbo <b.wordsworth03@gmail.com>
2025-07-11 11:49:43 +0100

    Info display

--- qplot/windows/main.py
+++ qplot/windows/main.py
@@ -4,7 +4,8 @@ from PyQt5 import QtCore
 from . import (
     plot1d,
     plot2d,
-    MainList
+    RunList,
+    moreInfo,
     )
 # from .treeWidgets import MainList
 from qplot.datahandling import dataset
@@ -13,6 +14,7 @@ from qcodes.dataset import (
     initialise_or_create_database_at,
     load_by_id
     )
+from qcodes.dataset.sqlite.database import get_DB_location
 
 import os
 
@@ -23,48 +25,73 @@ class MainWindow(qtw.QMainWindow):
     def __init__(self):
         super().__init__()
        
-        #prevent auto delete of windows
-        self.windows = []
+        #vars
+        self.windows = [] #prevent auto delete of windows
+        self.ds = None
+        
         
         self.l = qtw.QVBoxLayout()
         
+        
+        #Menu
+        menu = self.menuBar()
+        fileMenu = menu.addMenu("&File")
+        
+        loadAction = qtw.QAction("&Load", self)
+        loadAction.setShortcut("Ctrl+L")
+        loadAction.triggered.connect(self.getfile)
+        fileMenu.addAction(loadAction)
+        
+        #File Picker
         self.l.addWidget(qtw.QLabel("File Directory:"))
         
         self.fileTextbox = qtw.QLineEdit("C:/Users/Benjamin Wordsworth/.qcodes/code/WN7C_first cooldown.db")
         self.fileTextbox.setReadOnly(True)
         self.l.addWidget(self.fileTextbox)
         
-        fileButton = qtw.QPushButton("Choose File Location")
-        fileButton.clicked.connect(self.getfile)
-        self.l.addWidget(fileButton)
-        
-        
+        #Run Display and plot
         sublayout = qtw.QHBoxLayout()
         
         sublayout.addWidget(qtw.QLabel("Run id:"))
-        # self.pltTextbox = qtw.QLineEdit()
-        # self.l.addWidget(self.pltTextbox)
-        
+
         pltbutton = qtw.QPushButton("Open Plots")
         pltbutton.clicked.connect(self.openRuns)
         sublayout.addWidget(pltbutton)
         self.l.addLayout(sublayout)
-        self.listInit = False
         
+        self.listWidget = RunList()
+        self.l.addWidget(self.listWidget)
+        self.listWidget.selected.connect(self.updateSelected)
         
+        self.infoBox = moreInfo()
+        self.l.addWidget(self.infoBox)
+        
+        #Final Setup
         w = qtw.QWidget()
         w.setLayout(self.l)
         self.setCentralWidget(w)
        
+        self.resize(750, 600)
+        
         #bring window to top
         self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
         self.show() 
         self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowStaysOnTopHint) 
         self.show()
         
+###############################################################################
+#Open/Close events
+
     def closeEvent(self, event):
         qtw.QApplication.closeAllWindows()
-    
+        
+        
+    @QtCore.pyqtSlot(object)
+    def onClose(self, win):
+        self.windows.remove(win)
+        del win
+        # print(f"Closed {str(win)}, remaining: {self.windows}")
+            
     
     def openWin(self, widget, *args, **kargs):
         win = widget(*args, **kargs)
@@ -72,56 +99,76 @@ class MainWindow(qtw.QMainWindow):
         win.sig.connect(self.onClose)
         win.show()
 
-    @QtCore.pyqtSlot(object)
-    def onClose(self, win):
-        self.windows.remove(win)
-        del win
-        # print(f"Closed {str(win)}, remaining: {self.windows}")
-        
+
     
-    def openPlot(self, ds):
+    def openPlot(self):
         # ds = self.openDataset()
         
-        for param in ds.get_parameters():
+        for param in self.ds.get_parameters():
             if param.depends_on != "":
-                depends_on = param.depends_on.split(", ")
+                depends_on = param.depends_on_
                 if len(depends_on) == 1:
-                    self.openWin(plot1d, ds, param)
+                    self.openWin(plot1d, self.ds, param)
                 elif len(depends_on) == 2:
-                    self.openWin(plot2d, ds, param)
+                    self.openWin(plot2d, self.ds, param)
                 else:
                     raise IndexError(
                         f"Parameter: {param.name}, depends on too many variables ({depends_on}, {len(depends_on)=})"
-                        )
+                       )
         
-    def getfile(self):
-      filename = qtw.QFileDialog.getOpenFileName(self, 'Open file', 
-         os.getcwd(),"Data Base File (*.db)")[0]
-      self.fileTextbox.setText(filename)
-      initialise_or_create_database_at(os.path.abspath(filename))
-      
-      self.listWidget = MainList() 
-      if not self.listInit:
-          self.listWidget.selected.connect(self.updateSelected)
-          self.l.addWidget(self.listWidget)
-          
-          self.listInit = True
-
-
-    @QtCore.pyqtSlot(list)
-    def updateSelected(self, items):
-        self.selected = items
+###############################################################################
+#Button signals 
 
     @QtCore.pyqtSlot()
-    def openRuns(self):
-        try:
-            for run_id in self.selected:
-                ds = load_by_id(run_id)
-                self.openPlot(ds)
-        except AttributeError as error:
-            pass
+    def getfile(self):
+        filename = qtw.QFileDialog.getOpenFileName(self, 
+                                                   'Open file', 
+                                                   os.getcwd(),
+                                                   "Data Base File (*.db)"
+                                                   )[0]
+        self.fileTextbox.setText(filename)
+        if os.path.isfile(get_DB_location()):
+            initialise_or_create_database_at(os.path.abspath(filename))
+          
+        self.listWidget.refresh()
         
+        
+    @QtCore.pyqtSlot()
+    def openRuns(self):
+        assert self.ds is not None
+        self.openPlot()
+
+        
+    @QtCore.pyqtSlot(list)
+    def updateSelected(self, items):
+        self.ds = load_by_id(items[0])
+        
+        if hasattr(self.ds, "snapshot"):
+            snap = self.ds.snapshot
+        else:
+            snap = None
+        
+        paramspec = self.ds.get_parameters()
+        structure = {}
+        for param in paramspec:
+            if len(param.depends_on) > 0:
+                structure[param.name] = {"unit" : param.unit,
+                                         "label" : param.label,
+                                         "axes" : list(param.depends_on_)
+                                         }
+            else:
+                structure[param.name] = {"unit" : param.unit,
+                                         "label" : param.label
+                                         }
+        info = {"Data Structure" : structure,
+                "MetaData" : self.ds.metadata,
+                "Snapshot" : snap
+                }
+        self.infoBox.setInfo(info)
     
+###############################################################################        
+#Depreicated
+
     def openDataset(self):
         run_id = int(self.pltTextbox.text())
         ds = dataset.init_and_load_by_spec(
--- qplot/windows/treeWidgets.py
+++ qplot/windows/treeWidgets.py
@@ -11,21 +11,79 @@ from PyQt5 import (
 
 from ..datahandling import get_runs_from_db
 
+from qcodes.dataset.sqlite.database import get_DB_location
+
+from os.path import isfile
+
 import numpy as np
 
-class MainList(qtw.QListWidget):
+
+class RunList(qtw.QListWidget):
     
     selected = QtCore.pyqtSignal([list])
     
-    def __init__(self, *args, **kargs):
+    def __init__(self, *args, initalize=False, **kargs):
         super().__init__(*args, **kargs)
+        
+        if isfile(get_DB_location()):
+            runs = np.array(get_runs_from_db(), dtype=str)
+            self.addItems(runs)
+            
+        self.itemSelectionChanged.connect(self.onSelect)
+    
+    
+    def refresh(self):
+        self.clear()
         
         runs = np.array(get_runs_from_db(), dtype=str)
         self.addItems(runs)
-        
-        self.itemSelectionChanged.connect(self.onSelect)
-        
+
+    
+    
     @QtCore.pyqtSlot()
     def onSelect(self):
         selection = [item.text() for item in self.selectedItems()]
         self.selected.emit(selection)
+        
+        
+
+
+
+#taken from plottr
+class moreInfo(qtw.QTreeWidget):
+    
+    def __init__(self, *args):
+        super().__init__(*args)
+        
+        self.setHeaderLabels(["Key", "Value"])
+        self.setColumnCount(2)
+        
+    # @QtCore.pyqtSlot(dict)
+    def setInfo(self, info):
+        self.clear()
+        
+        items = dictToTree(info)
+        for item in items:
+            self.addTopLevelItem(item)
+            item.setExpanded(True)
+            
+        self.expandAll()
+        for i in range(2):
+            self.resizeColumnToContents(i)
+
+
+
+def dictToTree(d : dict):
+    items = []
+    for k, v in d.items():
+        if not isinstance(v, dict):
+            item = qtw.QTreeWidgetItem([str(k), str(v)])
+        else:
+            item = qtw.QTreeWidgetItem([k, ''])
+            for child in dictToTree(v):
+                item.addChild(child)
+        items.append(item)
+    return items
+        
+        
+        
